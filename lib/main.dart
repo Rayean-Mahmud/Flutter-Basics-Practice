1fc158c1560957115140c304ab6257c90d552661
Rayean-Mahmud <rayean.mahmud.arnob@gmail.com>
2023-02-20 18:25:29 +0600

    text inside container example

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("My App"),
+        ),
+        body: Container(
+          child: const Text("Hola! This text is inside a container."),
+        ),
+      ),
+    );
+  }
+}
